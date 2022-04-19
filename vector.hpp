cac943564e733ec359f3c4e2a415c2eb1c5748d1
Anderson <ander_alvarenga@hotmail.com>
2022-04-18 21:03:16 -0300

    starting vector

---
+++ vector.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalcara- <aalcara-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 18:06:09 by aalcara-          #+#    #+#             */
+/*   Updated: 2022/04/18 18:14:32 by aalcara-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <memory>
+
+template < class T, class Alloc = std::allocator<T> >
+class vector
+{
+	
+};
+
+#endif
